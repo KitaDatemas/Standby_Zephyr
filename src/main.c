ccf66ac24ca5706dd5dc03248fd059dc98e80b06
Ho Quoc Dat <dat.hoquocmt@gmail.com>
2025-02-24 20:41:40 +0700

    First commit

--- src/main.c
+++ src/main.c
@@ -7,41 +7,48 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+// #include <zephyr/drivers/sensor.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/__assert.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define LEDRED_NODE DT_ALIAS(led0)
 
 /*
  * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
+ * See the sample documentation for informat			ion on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(LEDRED_NODE, gpios);
 
 int main(void)
 {
 	int ret;
 	bool led_state = true;
-
-	if (!gpio_is_ready_dt(&led)) {
+	printk("Hello");
+	if (!gpio_is_ready_dt(&led_red)) {
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
+		ret = gpio_pin_toggle_dt(&led_red);
 		if (ret < 0) {
 			return 0;
 		}
 
 		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		printk("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
